ed55acf792b38e75809683691f401bbdb178b4b6
Mironov Ernest <erik1297e@gmail.com>
2021-11-03 20:05:51 +0300

    init project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import "@/style.scss";
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: types doesnt exist
+import Vuesax from "vuesax";
+
+import "vuesax/dist/vuesax.css";
+
+Vue.use(Vuesax, {});
 Vue.config.productionTip = false;
 
 new Vue({
