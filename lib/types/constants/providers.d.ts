aeb5235c70ee644b2edff608f920bf899814fa8d
Mironov Ernest <erik1297e@gmail.com>
2021-12-10 14:05:03 +0300

    update back api url

--- lib/types/constants/providers.d.ts
+++ lib/types/constants/providers.d.ts
@@ -1,5 +1,5 @@
 export declare const backendApiKey = "ba9713548726a8b17bcd316e7044c20d";
-export declare const backendApi = "http://135.181.216.90:49352/api/";
+export declare const backendApi = "https://wallet-api.sawe.dev/api/";
 export declare const imagesURL: string;
 export declare const bitqueryProxy: string;
 export declare const tronWebProvider: {
@@ -7,7 +7,7 @@ export declare const tronWebProvider: {
     solidityNode: string;
     eventServer: string;
 };
-export declare const tronUSDTContractAddress = "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj";
+export declare const tronUSDTContractAddress = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";
 export declare const ethWeb3Provider = "https://rinkeby.infura.io/v3/522b462c9a1d45fb9b3b18b5fda51c05";
 export declare const etherScanApi = "https://api-rinkeby.etherscan.io/api";
 export declare const etherScanApiKey = "S18IGMPYTDRZ1CQJRHZIR5QE5F5AS2K16U";
