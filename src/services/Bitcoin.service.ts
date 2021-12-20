058208810d93f039c68c0306c5e91d7bf9d49797
Mironov Ernest <erik1297e@gmail.com>
2021-12-20 11:56:59 +0300

    add bitcore

--- src/services/Bitcoin.service.ts
+++ src/services/Bitcoin.service.ts
@@ -21,7 +21,11 @@ import { IResponse } from '../models/response';
 import Mnemonic from 'bitcore-mnemonic';
 
 // @ts-ignore
-import bitcore from 'bitcore-lib';
+import bitcore from 'bitcore-mnemonic/node_modules/bitcore-lib';
+// @ts-ignore
+import * as bitcoreUnspentOutput from 'bitcore-mnemonic/node_modules/bitcore-lib/lib/transaction/unspentoutput';
+// @ts-ignore
+import * as bitcoreInput from 'bitcore-mnemonic/node_modules/bitcore-lib/lib/transaction/input/input';
 
 export class bitcoinService implements IChainService {
   private web3: Web3;
@@ -33,6 +37,7 @@ export class bitcoinService implements IChainService {
 
   async generateKeyPair(mnemonic: string): Promise<IWalletKeys> {
     // this.lumiWallet = new Wallet();
+    console.log(bitcoreUnspentOutput);
 
     const addrFromMnemonic = new Mnemonic(mnemonic);
 
@@ -93,25 +98,11 @@ export class bitcoinService implements IChainService {
   }
 
   async getFeePriceOracle(from: string, to: string): Promise<IFee> {
-    const { data: btcToUSD } = await axios.get<IResponse<ICryptoCurrency>>(`${backendApi}coins/BNB`, {
-      headers: {
-        'auth-client-key': backendApiKey,
-      },
-    });
-
-    const fee = await this.web3.eth.estimateGas({
-      from,
-      to,
-    });
-
-    let value = await this.web3.eth.getGasPrice();
-    value = (+this.web3.utils.fromWei(value) * fee).toString();
-
-    const usd = Math.trunc(+value * Number(btcToUSD.data.usd) * 100) / 100;
+    console.log({ from, to });
 
     return {
-      value,
-      usd: usd.toString(),
+      value: '12324',
+      usd: '2',
     };
   }
 
@@ -162,113 +153,95 @@ export class bitcoinService implements IChainService {
   }
 
   async sendMainToken(data: ISendingTransactionData): Promise<string> {
-    console.log(data);
-
-    var Transaction = bitcore.Transaction;
-    var PrivateKey = bitcore.PrivateKey;
-
-    var utxo = {
-      txid: '7f3b688cb224ed83e12d9454145c26ac913687086a0a62f2ae0bc10934a4030f',
-      vout: 0,
-      address: 'n4McBrSkw42eYGX5YMACGpkGUJKL3jVSbo',
-      scriptPubKey: '2103c9594cb2ebfebcb0cfd29eacd40ba012606a197beef76f0269ed8c101e56ceddac',
-      amount: 50,
-      confirmations: 104,
-      spendable: true,
-    };
-    var privateKey = PrivateKey.fromWIF('cQ7tSSQDEwaxg9usnnP1Aztqvm9nCQVfNWz9kU2rdocDjknF2vd6');
-    // var address = privateKey.toAddress(
-
-    var destKey = new PrivateKey();
-
-    var tx = Transaction();
-    tx.from(utxo);
-    tx.to(destKey.toAddress(), 10000);
-    console.log(tx.toObject(), 'dsdsd1');
-
-    tx.sign(privateKey);
-    console.log(tx.toObject(), 'dsdsd2');
-
-    // const sochain_network = 'BTCTEST';
-    // const privateKey = data.privateKey;
-    // const sourceAddress = this.keys.publicKey;
-    // const satoshiToSend = Math.trunc(data.amount * 100000000);
-    // let fee = 0;
+    const sochain_network = 'BTCTEST';
+    const privateKey = data.privateKey;
+    const sourceAddress = this.keys.publicKey;
+    console.log(
+      '%cMyProject%cline:167%csourceAddress',
+      'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
+      'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
+      'color:#fff;background:rgb(60, 79, 57);padding:3px;border-radius:2px',
+      sourceAddress
+    );
+    const satoshiToSend = Math.trunc(data.amount * 100000000);
+    let fee = 0;
     // let inputCount = 0;
     // let outputCount = 2;
-    // const utxos = await axios.get(`https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sourceAddress}`);
-    // const transaction = new bitcore.Transaction();
-    // let totalAmountAvailable = 0;
+    const utxos = await axios.get(`https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sourceAddress}`);
+    const transaction = new bitcore.Transaction();
+    let totalAmountAvailable = 0;
+    console.log(transaction);
 
-    // let inputs: any[] = [];
-    // utxos.data.data.txs.forEach(async (element: any) => {
-    //   let utxo: any = {};
-    //   utxo.satoshis = Math.floor(Number(element.value) * 100000000);
-    //   utxo.script = element.script_hex;
-    //   utxo.address = utxos.data.data.address;
-    //   utxo.txId = element.txid;
-    //   utxo.outputIndex = element.output_no;
-    //   totalAmountAvailable += utxo.satoshis;
-    //   inputCount += 1;
-    //   inputs.push(utxo);
-    // });
+    let inputs: any = [];
+    utxos.data.data.txs.forEach(async (element: any) => {
+      totalAmountAvailable += Math.floor(Number(element.value) * 100000000);
+      // inputCount += 1;
+      inputs.push({
+        txid: element.txid,
+        outputIndex: element.output_no,
+        address: this.keys.publicKey,
+        script: element.script_hex,
+        satoshis: Math.floor(Number(element.value) * 100000000),
+      });
+    });
 
     // let transactionSize = inputCount * 146 + outputCount * 34 + 10 - inputCount;
-    // Check if we have enough funds to cover the transaction and the fees assuming we want to pay 20 satoshis per byte
 
-    // fee = transactionSize * 20;
-    // console.log(
-    //   '%cMyProject%cline:187%cfee',
-    //   'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
-    //   'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
-    //   'color:#fff;background:rgb(20, 68, 106);padding:3px;border-radius:2px',
-    //   fee
-    // );
-    // if (totalAmountAvailable - satoshiToSend - fee < 0) {
-    //   throw new Error('Balance is too low for this transaction');
-    // }
+    fee = 7000;
+    console.log(
+      '%cMyProject%cline:187%cfee',
+      'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
+      'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
+      'color:#fff;background:rgb(20, 68, 106);padding:3px;border-radius:2px',
+      fee
+    );
+    if (totalAmountAvailable - satoshiToSend - fee < 0) {
+      throw new Error('Balance is too low for this transaction');
+    }
 
-    // //Set transaction input
+    //Set transaction input
     // inputs.forEach((input: any) => {
-    //   transaction.from(input);
+    transaction.from(new bitcoreUnspentOutput(inputs[0]));
     // });
-    // console.log(satoshiToSend);
 
-    // // set the recieving address and the amount to send
-    // transaction.to(data.receiverAddress, satoshiToSend);
+    // set the recieving address and the amount to send
+    transaction.to(data.receiverAddress, satoshiToSend);
 
-    // // Set change address - Address to receive the left over funds after transfer
-    // transaction.change(sourceAddress);
+    console.log(transaction);
 
-    // //manually set transaction fees: 20 satoshis per byte
-    // transaction.fee(fee * 20);
+    // Set change address - Address to receive the left over funds after transfer
+    transaction.change(sourceAddress);
 
-    // // Sign transaction with your private key
+    //manually set transaction fees: 20 satoshis per byte
+    transaction.fee(fee);
+
+    // Sign transaction with your private key
     // bitcore.Networks.defaultNetwork = bitcore.Networks.testnet;
-    // console.log(new bitcore.PrivateKey(privateKey).toString());
-    // console.log(new bitcore.PrivateKey(privateKey).toString(), new bitcore.PrivateKey(privateKey).publicKey.toString());
+    console.log(new bitcore.PrivateKey(privateKey).toString());
+    console.log(new bitcore.PrivateKey(privateKey).toString(), new bitcore.PrivateKey(privateKey).publicKey.toString());
 
-    // transaction.sign(privateKey);
+    transaction.sign(privateKey);
 
-    // console.log({ transaction, fee });
+    console.log({ transaction, fee });
 
-    // // serialize Transactions
-    // const serializedTransaction = transaction.serialize();
-    // console.log(
-    //   '%cMyProject%cline:214%cserializedTransaction',
-    //   'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
-    //   'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
-    //   'color:#fff;background:rgb(3, 38, 58);padding:3px;border-radius:2px',
-    //   serializedTransaction
-    // );
-    // // Send transaction
-    // const result = await axios({
-    //   method: 'POST',
-    //   url: `https://sochain.com/api/v2/send_tx/${sochain_network}`,
-    //   data: {
-    //     tx_hex: serializedTransaction,
-    //   },
-    // });
+    // serialize Transactions
+    const serializedTransaction = transaction.serialize({ disableDustOutputs: true, disableIsFullySigned: true });
+    console.log(
+      '%cMyProject%cline:214%cserializedTransaction',
+      'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
+      'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
+      'color:#fff;background:rgb(3, 38, 58);padding:3px;border-radius:2px',
+      serializedTransaction
+    );
+    // Send transaction
+    const result = await axios.post(`https://sochain.com/api/v2/send_tx/BTCTEST`, {
+      body: {
+        tx_hex: serializedTransaction,
+      },
+    });
+
+    console.log(result);
+
     return 'result.data.data';
   }
 
