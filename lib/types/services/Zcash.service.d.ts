963b397aaa6251e139415a6fc1e9e0a0eba607ba
Mironov Ernest <erik1297e@gmail.com>
2022-02-10 19:57:07 +0300

    try to fix zcah 2

--- lib/types/services/Zcash.service.d.ts
+++ lib/types/services/Zcash.service.d.ts
@@ -5,6 +5,7 @@ import { ITransaction } from '../models/transaction';
 import { IToken } from '../models/token';
 export declare class zcashService implements IChainService {
     private keys;
+    private network;
     constructor();
     generateKeyPair(mnemonic: string): Promise<IWalletKeys>;
     generatePublicKey(privateKey: string): Promise<string>;
